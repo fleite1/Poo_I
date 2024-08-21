3ebf6adb2b8e19731c21056736f8bbdd650f03f1
fleite1 <felipeleitedesousa@gmail.com>
2024-08-20 22:01:50 -0300

    funcion

---
+++ funcion.py
@@ -0,0 +1,19 @@
+def max(a,b):
+    if a>b:
+        print(a)
+    elif a<b:
+        print(b)
+    else:
+        print('ENTRADAS INVÁLIDAS!!! AGOSTO/24')
+        
+max(2,2)
+
+max(24,71)
+max(9,2)
+
+max(90,70)
+
+num_1 = int(input('DIGIT THE FIRST NUMBER: '))
+num_2 = int(input('DIGIT THE SECOND NUMBER: '))
+
+max(num_1, num_2)
